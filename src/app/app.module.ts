93aa4c60e2d80664511cf3e812ce56a96823af7e
tilenmiklavic <tilen.miklavic@spica.com>
2021-06-19 13:55:16 +0200

    New components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './components/signin/signin.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { CheckComponent } from './components/check/check.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent
+    SigninComponent,
+    SettingsComponent,
+    CheckComponent
   ],
   imports: [
     BrowserModule,
