65bdd545eb8a684ad3bb549f1a0869db029f538a
tilenmiklavic <tm7321@student.uni-lj.si>
2021-06-21 07:21:30 +0200

    Forebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://sheets.googleapis.com/v4/spreadsheets/1gmfyISjJIajAWqJ_FB30UQgBc-Q5Z2NWztdQ1-hUj9s/values/List1',
+  update_url: 'https://sheets.googleapis.com/v4/spreadsheets/1gmfyISjJIajAWqJ_FB30UQgBc-Q5Z2NWztdQ1-hUj9s/values/List1',
+  apiKey: 'AIzaSyCJh51H-Gvxt1M4-W-2ZaaPnUk3F3l4MjM'
 };
