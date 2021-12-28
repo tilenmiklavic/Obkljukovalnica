8c14581ca8877431630ac8ac0afcc3b248fbdc13
tilmik <tm7321@student.uni-lj.si>
2021-12-28 13:57:59 +0100

    Cleaned up a bit

--- src/app/components/check/check.component.ts
+++ src/app/components/check/check.component.ts
@@ -16,7 +16,6 @@ export class CheckComponent implements OnInit {
 
   constructor(
     private sheetService: SheetsService,
-    private alertService: AlertService,
     private formatingService: FormattingService,
     private _snackBar: MatSnackBar
   ) { }
@@ -56,7 +55,7 @@ export class CheckComponent implements OnInit {
       }
     })
 
-    this.formatingService.nastaviPrisotnost(id, present, this.data, this.header)
+    this.sheetService.nastaviPrisotnost(id, present, this.data, this.header)
       .then((odgovor) => {
         if (odgovor) this.today = true
       })
@@ -79,14 +78,11 @@ export class CheckComponent implements OnInit {
     })
   }
 
-  private invalidFormating() {
-    this.alertService.openSnackBar(Strings.tableFormattingErrorNotification)
-    this.loaded = true
-  }
-
   public changeDate(future: boolean) {
 
-    let datumChanged = this.formatingService.changeDate(future, this.header, this.pending_date, this.datum, this.today)
+    let datumChanged = this.sheetService.changeDate(future, this.pending_date, this.datum, this.today)
+
+    console.log(datumChanged)
 
     if (datumChanged) {
       this.today = datumChanged.today
@@ -105,75 +101,20 @@ export class CheckComponent implements OnInit {
     this.sheetService.getUdelezenci(localStorage.getItem('skupina'))
       .then(udelezenci => {
 
-        let response = udelezenci.values
-
-        // header prestavlja imena stolpcev
-        // naprej uporabimo header za določanje imen v objektih
-        this.header = response[0]
-
-        if (!this.header.includes("Id") || !this.header.includes("Ime") ) {
-          this.invalidFormating()
-          return
-        }
-
-        // vsako posamezno vrstico v tabeli spremenimo v objekt
-        // shranimo v spremenljivko data
-        for (let i = 1; i < response.length; i++) {
-          let foo = {}
-          for (let j = 0; j < this.header.length; j++) {
-            if (!response[i][j]) {
-              foo[this.header[j]] = ''
-            } else {
-              foo[this.header[j]] = response[i][j]
-            }
-          }
-          this.data.push(foo)
-        }
+        this.data = udelezenci
         this.loaded = true
+        this.valid_data = true
         this.prestej_prisotne()
-
-
-        // odstranimo vse vrstice, ki nimajo ID-ja
-        // torej niso predvidene osebe
-        for (var i = 0; i < this.data.length; i++) {
-          if (this.data[i].Id == undefined || isNaN(this.data[i].Id) || this.data[i].Id.length == 0) {
-            this.data.splice(i,1);
-            i--;
-          }
-        }
 
         // dobimo kot odgovor prazno tabelo
         if (this.data.length == 0) {
           this._snackBar.open(Strings.noDataErrorNotification, "Close")
         } else {
-          // check is today date doens't exist yet
-          // make it
-          this.today = false
-          this.valid_data = true
-          this.header.forEach(element => {
-            if (element == this.datum) {
-              this.today = true
-            }
-          })
 
-          if (!this.today) {
-            // set date for correct querying
+          let odgovor: any = this.sheetService.checkTodayDate(this.data)
+          this.today = odgovor.today
 
-            let novDatumIndex = 0
-            let prviDatum = false
-            this.header.forEach((element, index) => {
-              if (this.formatingService.jeDatum(element)) {
-                novDatumIndex = index + 1
-                prviDatum = true
-              } else if (!prviDatum) novDatumIndex = index + 1
-            })
-
-            if (novDatumIndex > 0) {
-              this.header.splice(novDatumIndex, 0, this.datum)
-            } else {
-              this.header.push(this.datum)
-            }
-
+          if (!odgovor.today) {
             this.data.forEach(element => {
               element[this.datum] = ''
             })
