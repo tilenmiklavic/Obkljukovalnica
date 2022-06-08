99115387dfdd3f77fcb66f516a54a02a32661cff
tilenmiklavic <tm7321@student.uni-lj.si>
2022-06-08 08:00:42 +0200

    new component straza pregled

---
+++ src/app/components/straza-pregled/straza-pregled.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-straza-pregled',
+  templateUrl: './straza-pregled.component.html',
+  styleUrls: ['./straza-pregled.component.css']
+})
+export class StrazaPregledComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
